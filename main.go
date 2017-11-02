952d1523e5d428ee6784ff5954ed39cfbe0a128a
lorentzca <knsmkt2943@gmail.com>
2017-11-02 12:43:25 +0900

    Split main()

--- main.go
+++ main.go
@@ -9,17 +9,26 @@ import (
 	"github.com/gorhill/cronexpr"
 )
 
-func main() {
-	stdin := bufio.NewScanner(os.Stdin)
+func getCrontab(stdin *bufio.Scanner) []string {
 	var crontab []string
 	for stdin.Scan() {
 		crontab = append(crontab, stdin.Text())
 	}
+	return crontab
+}
 
+func getNextTime(crontab []string) []time.Time {
 	var nextTime []time.Time
 	for _, v := range crontab {
 		nextTime = append(nextTime, cronexpr.MustParse(v).Next(time.Now()))
 	}
+	return nextTime
+}
+
+func main() {
+	stdin := bufio.NewScanner(os.Stdin)
+	crontab := getCrontab(stdin)
+	nextTime := getNextTime(crontab)
 
 	for _, v := range nextTime {
 		fmt.Println(v)
